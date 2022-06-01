fe30e8ef6e95a80f0f954d5d6cda6792830d6ad4
Hubert Chudek <hubert.chudek@accenture.com>
2022-06-01 11:12:59 +0200

    Add parser middleware and basic input validation.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,14 @@
 // @route /api/users
 // @access Public
 const registerUser = (req, res) => {
+    const { name, email, password } = req.body
+
+    //Validation
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('All fields have to be filled!')
+    }
+
     res.send('Register route')
 }
 
