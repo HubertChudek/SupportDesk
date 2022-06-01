acc95e82d27ad9e18563c190e8e52f9ee88d16a9
Hubert Chudek <hubert.chudek@accenture.com>
2022-06-01 11:34:05 +0200

    Create error handler. Add some comments.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config
+const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+//Middleware config - parsers
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+//Root response
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to Support the Desk API!' })
 })
@@ -14,4 +17,8 @@ app.get('/', (req, res) => {
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 
+//Middleware config - errors handling
+app.use(errorHandler)
+
+//Listening port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
