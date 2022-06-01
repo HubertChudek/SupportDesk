1636b6842b2931348ddbe927f5251186b2f357cc
Hubert Chudek <hubert.chudek@accenture.com>
2022-06-01 10:01:08 +0200

    Add routes and controller file.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).json({message: 'Welcome to Support the Desk API!'})
+    res.status(200).json({ message: 'Welcome to Support the Desk API!' })
 })
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
