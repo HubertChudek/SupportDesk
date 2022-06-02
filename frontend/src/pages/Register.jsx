8be7e1082d573baa0141526358ebdb44048b9ff4
Hubert Chudek <hubert.chudek@accenture.com>
2022-06-02 12:09:03 +0200

    Create register form.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import { React, useState } from 'react'
+import { toast } from 'react-toastify'
+import { FaUser } from 'react-icons/fa'
 
 function Register() {
-  return <h1>Register</h1>;
+  const [formData, setFormatData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  const { name, email, password, password2 } = formData
+
+  const onChange = (e) => {
+    setFormatData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== password2) {
+      toast.error('Passwords do not match!')
+    }
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <sec className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name} // Has to be same as above 'name' prop
+              onChange={onChange}
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email} // Has to be same as above 'name' prop
+              onChange={onChange}
+              placeholder="Enter your email address"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password} // Has to be same as above 'name' prop
+              onChange={onChange}
+              placeholder="Enter password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2} // Has to be same as above 'name' prop
+              onChange={onChange}
+              placeholder="Repeat password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </sec>
+    </>
+  )
 }
 
-export default Register;
+export default Register
