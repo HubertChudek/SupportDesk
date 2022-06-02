e7afa8c3a426198a58d628ab5e2bcacac81843a7
Hubert Chudek <hubert.chudek@accenture.com>
2022-06-02 12:13:21 +0200

    Create login form.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { React, useState } from 'react'
+import { toast } from 'react-toastify'
+import { FaSignInAlt } from 'react-icons/fa'
 
 function Login() {
-  return <h1>Login</h1>;
+  const [formData, setFormatData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onChange = (e) => {
+    setFormatData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please loggin to get support</p>
+      </section>
+      <sec className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email} // Has to be same as above 'name' prop
+              onChange={onChange}
+              placeholder="Enter your email address"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Enter password"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </sec>
+    </>
+  )
 }
 
-export default Login;
+export default Login
