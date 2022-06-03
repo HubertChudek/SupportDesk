581f073c47bcef7a0f29f39ca232c53d9b68ee53
Hubert Chudek <hubert.chudek@accenture.com>
2022-06-03 11:49:14 +0200

    Connect reducer to Login form.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { React, useState } from 'react'
 import { toast } from 'react-toastify'
 import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../features/auth/authSlice'
 
 function Login() {
   const [formData, setFormatData] = useState({
@@ -9,6 +11,12 @@ function Login() {
   })
 
   const { email, password } = formData
+
+  const dispatch = useDispatch()
+  // Getting data from state and destructurizing it
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
 
   const onChange = (e) => {
     setFormatData((prevState) => ({
@@ -19,6 +27,13 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
   }
 
   return (
