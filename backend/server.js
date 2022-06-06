ac7701ee3416f93bd5f8a1470e8cd53d5017bdaf
Hubert Chudek <hubert.chudek@accenture.com>
2022-06-06 15:29:07 +0200

    Create ticket routes and controller.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const PORT = process.env.PORT || 5000
 
@@ -18,11 +18,12 @@ app.use(express.urlencoded({ extended: false }))
 
 //Root response
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to Support the Desk API!' })
+  res.status(200).json({ message: 'Welcome to Support the Desk API!' })
 })
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 //Middleware config - errors handling
 app.use(errorHandler)
