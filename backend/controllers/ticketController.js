db7bd62ccd8551b5d08ec907c16024f0050e41ca
Hubert Chudek <hubert.chudek@accenture.com>
2022-06-07 11:27:57 +0200

    Add ticket delete and update routes and controllers.

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -41,6 +41,64 @@ const getTicket = asyncHandler(async (req, res, next) => {
   res.status(200).json(ticket)
 })
 
+// @desc Delete particular ticket
+// @route DELETE /api/tickets/:id
+// @access Private
+const deleteTicket = asyncHandler(async (req, res, next) => {
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  // Validate if the ticket is owned by authenticated user (aka authorize user to access this ticket)
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(403)
+    throw new Error('Not authorized')
+  }
+
+  await ticket.remove()
+
+  res.status(200).json({ success: true })
+})
+
+// @desc Update user's ticket
+// @route PUT /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler(async (req, res, next) => {
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  // Validate if the ticket is owned by authenticated user (aka authorize user to access this ticket)
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(403)
+    throw new Error('Not authorized')
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  )
+
+  res.status(200).json(updatedTicket)
+})
+
 // @desc Create new ticket
 // @route POST /api/tickets
 // @access Private
@@ -70,5 +128,7 @@ const createTicket = asyncHandler(async (req, res, next) => {
 module.exports = {
   getTickets,
   getTicket,
+  updateTicket,
+  deleteTicket,
   createTicket,
 }
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,10 +4,16 @@ const protect = require('../middleware/authMiddleware')
 const {
   getTickets,
   getTicket,
+  updateTicket,
+  deleteTicket,
   createTicket,
 } = require('../controllers/ticketController')
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
-router.route('/:id').get(protect, getTicket)
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .put(protect, updateTicket)
+  .delete(protect, deleteTicket)
 
 module.exports = router
